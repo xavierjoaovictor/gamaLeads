98d345ea4f490f3291ffe5d88efe025c37f14334
Joao Xavier <xavier.joaovictor@gmail.com>
2017-07-17 17:35:23 -0300

    Need to change vimeo video

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 var signupForm = document.getElementById('signup-form');
 var leadIP;
-var apiKey = "test_2a6e5cb862c01534ff3c38c6f4fd1bf8d6bc0f837ea8150932a768315f37d276"
 
 $.getJSON('https://freegeoip.net/json/?callback=?', function(data) {
   leadIP = data;
-  // console.log(leadIP);
+  console.log(leadIP);
 });
 
-var onSignupComplete = function(error) {
-  if (error) {
-    console.log(error)
-  } 
+var SPMaskBehavior = function (val) {
+  return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+},
+spOptions = {
+  onKeyPress: function(val, e, field, options) {
+      field.mask(SPMaskBehavior.apply({}, arguments), options);
+    }
 };
 
+$('#celphone').mask(SPMaskBehavior, spOptions);
+
 function signup(formObj) {
+
+    var date = new Date();
 
     // Referencia "tabela" existente ou cria uma nova
     var firebaseTable = new Firebase("https://gamaleads-d0573.firebaseio.com/signups");
@@ -23,8 +29,10 @@ function signup(formObj) {
       Email: formObj.email.value,
       IP: leadIP.ip,
       Local: leadIP.city + '/' + leadIP.region_code,
-      Celular: formObj.celphone.value
-    }, onSignupComplete);
+      Celular: formObj.celphone.value,
+      Data: date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear(),
+      Hora: date.getHours() + ':' + date.getMinutes() + ':'+ date.getSeconds()
+    });
 
     return false;
 }
